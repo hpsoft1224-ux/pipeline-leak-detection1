837c96c1b93f190e04666765e0cc2488407cbe64
hpsoft1224-ux <hpsoft1224@gmail.com>
2026-09-26 11:52:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,12 +44,8 @@ def predict():
 
     threshold_alarm = int(deficit >= THRESHOLD)
 
-    if threshold_alarm == 1:
-        final_prediction = 1
-        status = "LEAK"
-    else:
-        final_prediction = 0
-        status = "NO LEAK"
+    final_prediction=int(rf_prediction == 1 or threshold-alarm ==1)
+    status = "LEAK" if final_prediction else "NO LEAK"
 
     return jsonify({
         "inlet_lpm": inlet,
